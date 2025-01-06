a87cdbc6af9d09ccba6a7a7f2a2047828162b540
Syifa Salsabila <146178436+ssalsabila0911@users.noreply.github.com>
2025-01-06 12:08:23 +0700

    Update Sewa.php

--- admin/application/controllers/Sewa.php
+++ admin/application/controllers/Sewa.php
@@ -28,35 +28,5 @@ class Sewa extends CI_Controller {
 
 
     public function notifSewa($id_sewa){
-        $sewa = $this->Msewa->detail_sewa($id_sewa);
-
-        $isipesan ="Dear penyewa ".$sewa["nama"]."<br>";
-        $isipesan.="Saat ini masa sewa anda untuk motor ".$sewa['motor']." dengan nomor ".$sewa['plat_motor']." sudah hampir habis.<br>";
-        $isipesan.="Silahkan kembalikan sebelum tanggal masa sewa habis ! <br>";
-        $isipesan.="- Admin sewamotormu - <br>";
-
-        $payload['data'][0]['phone'] = $sewa['no_tlp']; 
-        $payload['data'][0]['message'] = $isipesan;
-
-        $curl = curl_init();
-        $token = $this->config->item("token_wablas");
-        curl_setopt($curl, CURLOPT_HTTPHEADER, 
-            array(
-                "Authorization: U629wUFHi7AyhykPeWhIQD4DUaO1hEKa7jOvI6yZHHIlMI58Y2aZbmUCBW0iWOP3", 
-                "Content-Type: application/json"
-            )
-        );
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload) );
-        curl_setopt($curl, CURLOPT_URL, "https://jogja.wablas.com/api/v2/send-message");
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        $result = curl_exec($curl);
-        curl_close($curl);
-
-        $this->Msewa->set_notif($id_sewa);
-        $this->session->set_flashdata('pesan_sukses', 'Notifikasi Berhasil Dikirim !');
-        redirect('sewa','refresh');
-    }
+        //api disini
 }
